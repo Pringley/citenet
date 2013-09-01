3fe90bc6d83ac8c6f7b2b1e761cb755368b84810
Ben Pringle <ben.pringle@gmail.com>
2013-09-01 14:38:30 -0400

    Create setuptools skeleton

--- README.markdown
+++ README.markdown
@@ -2,3 +2,9 @@
 
 CiteNet contains Python implementations of several citation network algorithms
 for use with [NetworkX](http://networkx.github.io) graphs.
+
+## Installation
+
+To install (including all dependencies), simply run:
+
+    python setup.py install
---
+++ setup.py
@@ -0,0 +1,19 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+setup(
+    name = "CiteNet",
+    version = "0.0.1",
+    packages = find_packages(),
+    
+    install_requires = """
+      networkx>=1.8.1
+      scipy>=0.9.0
+    """,
+
+    author = "Ben Pringle",
+    author_email = "ben.pringle@gmail.com",
+    description = "Citation network analysis package",
+    license = "MIT",
+)
