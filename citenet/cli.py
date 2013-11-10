c424744af801241fbdced0e3344c1f9b6f2c6416
Ben Pringle <ben.pringle@gmail.com>
2013-11-09 21:23:25 -0500

    Add rudimentary JSON configuration to CLI

--- citenet/cli.py
+++ citenet/cli.py
@@ -1,5 +1,15 @@
 """Command-line interface for citenet."""
 
+import sys
+import json
+import citenet
+
 def main():
     """Run the CLI."""
-    print('CiteNet CLI')
+    if len(sys.argv) != 2:
+        print("Usage: {} <config_file>")
+
+    with open(sys.argv[1]) as config_file:
+        config = json.load(config_file)
+
+    graph = citenet.read_csv_graph(**config['graph'])
