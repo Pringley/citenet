55d951c42b0b58072b69fde954bd3f0d6273e573
Ben Pringle <ben.pringle@gmail.com>
2013-11-09 21:01:22 -0500

    Switch back from JSON to CSV
    Our native data is in CSV, so let's stick with that for now.

--- citenet/__init__.py
+++ citenet/__init__.py
@@ -1,5 +1,5 @@
 """Library for citation network analysis."""
 
 from citenet.neighborrank import neighborrank
-from citenet.io import read_json_graph, write_json_graph
+from citenet.io import read_csv_graph
 from citenet.util import top_n_from_dict
--- citenet/io.py
+++ citenet/io.py
@@ -1,27 +1,56 @@
 """Utilities for reading and writing citation graphs."""
 
-import gzip
-from networkx.readwrite import json_graph
+import csv
+import logging
+from networkx import DiGraph
 
-def write_json_graph(graph, filename, compressed=True):
-    """Write a graph to file."""
-    if compressed:
-        file_obj = gzip.open(filename, 'wb')
-    else:
-        file_obj = open(filename, 'w')
-    try:
-        json_graph.dump(graph, file_obj)
-    finally:
-        file_obj.close()
+def read_csv_graph(filename,
+                   delimiter=',',
+                   reverse_edges=False,
+                   suppress_warnings=False,
+                   ignore_header=False):
+    """Read an edgelist style graph from a CSV file.
 
-def read_json_graph(filename, compressed=True):
-    """Read a graph from file."""
-    if compressed:
-        file_obj = gzip.open(filename, 'rb')
-    else:
-        file_obj = open(filename)
-    try:
-        graph = json_graph.load(file_obj)
-    finally:
-        file_obj.close()
+    Options:
+        filename -- path to the csv file
+        delimiter -- CSV delimiter (e.g. ',', '\\t')
+        reverse_edges -- flag if edges aren't in (source, dest) order
+        suppress_warnings -- flag to ignore "bad line" warnings
+        ignore_header -- flag to ignore the first line in CSV file
+
+    """
+    logger = logging.getLogger(__name__)
+
+    edgeset = set()
+    with open(filename, newline='') as file_obj:
+        reader = csv.reader(file_obj, delimiter=delimiter)
+
+        # Skip header line (if specified).
+        if ignore_header:
+            _ = next(reader)
+
+        for row in reader:
+            pair = tuple(row if not reverse_edges
+                         else reversed(row))
+
+            def warn(message):
+                """Log a warning on this line."""
+                if suppress_warnings:
+                    return
+                full_message = 'Error in {} on line {}: {}'
+                logger.warn(full_message.format(filename,
+                                                reader.line_num,
+                                                message))
+
+            if not all(pair):
+                warn('Incomplete edge: {}'.format(pair))
+            elif pair in edgeset:
+                warn('Duplicate edge: {}'.format(pair))
+            else:
+                # If no errors, add the edge!
+                edgeset.add(pair)
+
+    graph = DiGraph()
+    graph.add_edges_from(edgeset)
+
     return graph
